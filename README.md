1a161478f9dde2bf41e3eebaa5d2dcfaaa5113fc
ezrajames <ezra@revela.io>
2022-10-17 21:28:17 -0700

    Initialize repository

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# Assignment 1
+
+This project provides scripts and functionality for Assignment 1 of the Data Models and Algorithms course at the University of Victoria.
+
+## MySQL
+
+This project uses MySQL which can be installed by following the instructions found [here](https://dev.mysql.com/doc/mysql-installation-excerpt/5.7/en/) for your respective operating system.
+
+## Install Project
+
+This project uses Python 3.9. If you do not have Python 3.9 explicitly installed on your machine, I recommend using a tool like [pyenv](https://github.com/pyenv/pyenv) to install it as it allows you to install and easily manage multiple Python versions and virtual environments. Once you have Python 3.9 installed, you can run the following command to install the project dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+## Git LFS
+
+This project uses Git LFS to store the large data files used in this repository. To install Git LFS, follow the instructions [here](https://git-lfs.github.com).
+To retrieve the remote data used in this project, run the following command:
+
+```bash
+git lfs fetch
+```
---
+++ sql/QuickDBD-MovieLens.sql
@@ -0,0 +1,50 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/qjAKsR
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Modify this code to update the DB schema diagram.
+-- To reset the sample schema, replace everything with
+-- two dots ('..' - without quotes).
+
+CREATE TABLE `Movie` (
+    `id` int  NOT NULL ,
+    `title` varchar(80)  NOT NULL ,
+    `genres` varchar(80)  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `User` (
+    `id` int  NOT NULL ,
+    PRIMARY KEY (
+        `id`
+    )
+);
+
+CREATE TABLE `MovieTag` (
+    `movie_id` int  NOT NULL ,
+    `user_id` int  NOT NULL ,
+    `name` varchar(80)  NOT NULL ,
+    `date_time` timestamp  NOT NULL 
+);
+
+CREATE TABLE `Rating` (
+    `user_id` int  NOT NULL ,
+    `movie_id` int  NOT NULL ,
+    `rating` smallint  NOT NULL ,
+    `date_time` timestamp  NOT NULL 
+);
+
+ALTER TABLE `MovieTag` ADD CONSTRAINT `fk_MovieTag_movie_id` FOREIGN KEY(`movie_id`)
+REFERENCES `Movie` (`id`);
+
+ALTER TABLE `MovieTag` ADD CONSTRAINT `fk_MovieTag_user_id` FOREIGN KEY(`user_id`)
+REFERENCES `User` (`id`);
+
+ALTER TABLE `Rating` ADD CONSTRAINT `fk_Rating_user_id` FOREIGN KEY(`user_id`)
+REFERENCES `User` (`id`);
+
+ALTER TABLE `Rating` ADD CONSTRAINT `fk_Rating_movie_id` FOREIGN KEY(`movie_id`)
+REFERENCES `Movie` (`id`);
+
