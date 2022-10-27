70b99b4d7e12d074ed5e04eca3dd99df0bbd39df
ezrajames <ezra@revela.io>
2022-10-26 19:25:33 -0700

    Add updated SQL for loading database

--- assignment_1/sql/create_tables.sql
+++ assignment_1/sql/create_tables.sql
@@ -5,35 +5,27 @@ CREATE TABLE Movie (
     PRIMARY KEY (id)
 );
 
-CREATE TABLE User (
-    id INT NOT NULL,
-    PRIMARY KEY (id)
-);
-
 CREATE TABLE Rating (
     user_id INT NOT NULL,
     movie_id INT NOT NULL,
     rating FLOAT,
     date_time TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES User(id),
     FOREIGN KEY (movie_id) REFERENCES Movie(id)
 );
 
 CREATE TABLE Tag (
     id INT NOT NULL,
-    tag VARCHAR(200),
+    name VARCHAR(200),
     score FLOAT,
-    label VARCHAR(15),
+    emotion VARCHAR(20),
     PRIMARY KEY (id)
 );
 
 CREATE TABLE MovieTag (
-    user_id INT NOT NULL,
     movie_id INT NOT NULL,
     tag_id INT NOT NULL,
+    user_id INT NOT NULL,
     date_time TIMESTAMP,
-    FOREIGN KEY (user_id) REFERENCES User(id),
     FOREIGN KEY (movie_id) REFERENCES Movie(id),
-    FOREIGN KEY (tag_id) REFERENCES Tag(id),
-    UNIQUE(tag)
+    FOREIGN KEY (tag_id) REFERENCES Tag(id)
 );
--- assignment_1/sql/load_db.sql
+++ assignment_1/sql/load_db.sql
@@ -7,52 +7,31 @@ LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
 (id, title, genres);
 
--- Load all users who have created a rating or a tag
+-- Load Ratings table with user creatd labels
 LOAD DATA LOCAL 
 INFILE "/home/macdonaldezra/Desktop/code/uvic/csci_501/assignment_1/data/ml-latest/ratings.csv" IGNORE
-INTO TABLE User
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 ROWS
-(id);
-
-LOAD DATA LOCAL 
-INFILE "/home/macdonaldezra/Desktop/code/uvic/csci_501/assignment_1/data/ml-latest/tags.csv" IGNORE
-INTO TABLE User
-FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
-IGNORE 1 ROWS
-(id);
-
-LOAD DATA LOCAL 
-INFILE "/home/macdonaldezra/Desktop/code/uvic/csci_501/assignment_1/data/ml-latest-small/ratings.csv" IGNORE
 INTO TABLE Rating
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
 LINES TERMINATED BY '\r\n'
 IGNORE 1 ROWS
-(user_id, movie_id, rating, @time)
-SET time = FROM_UNIXTIME(@time);
-
+(user_id, movie_id, rating, @t)
+SET date_time = FROM_UNIXTIME(@t);
 
 -- Load tags into Tag table
 LOAD DATA LOCAL 
-INFILE "/home/macdonaldezra/Desktop/code/uvic/csci_501/assignment_1/data/ml-latest/tags.csv" IGNORE
+INFILE "/home/macdonaldezra/Desktop/code/uvic/csci_501/assignment_1/data/ml-latest/computed-tags.csv"
 INTO TABLE Tag
 FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
-LINES TERMINATED BY '\r\n'
+LINES TERMINATED BY '\n'
 IGNORE 1 ROWS
-(id);
+(id, name, emotion, score);
 
--- LOAD DATA INFILE "./data/ml-latest/movies.csv"
--- INTO TABLE movie
--- FIELDS TERINATED BY ","
--- LINES TERMINATED BY "\n"
--- IGNORE 1 ROWS;
--- (movie_id, title, genres);
-
--- LOAD DATA INFILE "./data/ml-latest/movies.csv"
--- INTO TABLE movie
--- FIELDS TERINATED BY ","
--- LINES TERMINATED BY "\n"
--- IGNORE 1 ROWS;
--- (movie_id, title, genres);
+-- Load user created movie tags into the MovieTag table
+LOAD DATA LOCAL 
+INFILE "/home/macdonaldezra/Desktop/code/uvic/csci_501/assignment_1/ml-latest/indexed-tags.csv" IGNORE
+INTO TABLE MovieTag
+FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 ROWS
+(user_id, movie_id, tag_id, @t)
+SET date_time = FROM_UNIXTIME(@t);
